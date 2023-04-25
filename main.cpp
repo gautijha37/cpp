079df5a134057867a4e32e06d1ead4253861cc8d
Gautam Jha <gauti.jha37@gmail.com>
2023-04-25 22:06:20 +0530

    add travis support

--- main.cpp
+++ main.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <functional>
 
 int main(){
-	std::cout << "Hello world";
+//	std::cout << "Hello world"
+	auto my_lambda = [out = std::ref(std::cout << "Hello World")](){
+			out.get() << "of mine" << std::endl;
+		};
+	my_lambda();
 	return 0;
 }
